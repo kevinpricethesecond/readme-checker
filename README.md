419f373d84b47c07b8c6828265e21bd40b71f5b4
kevinpricethesecond <kevinpricethesecond@gmail.com>
2024-06-10 13:34:00 -0500

    working.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # readme-checker
 Gets a list of repos in dockerhub without a description. The repos must be public to be found by the script and a username and password must be passed as params. If no `--namespace` param is provided, it is assumed to be the username.
 
-Quickstart:
+## Quickstart:
+### checking readmes
+Use the below command to get a list of repos without a descriptiuon (overview)
 `$ python3 readme-checker.py --username <<dockerhub username>> --password <<dockerhub password>> --namespace <<dockerhub namespace>> --file <</path/to/file>>`
+response format:
 ```
 {
     "count": count,
@@ -35,3 +38,16 @@ Options
 --password Docker hub password *required
 --namespace The dockerhub namespace to search. Defaults to the username *optional
 --file Path to the output file. Defaults to None. If this is not provided, output will just print to console. *optional
+
+### updating readmes
+Use the below command to update a readme.
+`python3 readme-updater.py`
+
+You *must* have an .env file containing a dockerhub username/password with access to the repos you're trying to update. Example of this is in ./updater/.env-example
+You *must* have a map.json file with the following params:
+```
+"dockerhub_target" --> the repo in Dockerhub that you want to update, in the format docker.io/<namepsace>/<repo name>
+"readme_location" --> the url to the README.md file you want as the overview of the Dockerhub repo. This script will pull it down and put it in Docklerhub (using python requests.get).
+"short_desc" --> a short description of the repo. Usually optional.
+```
+an example of this is at ./updater/map.json
--- updater/readme-updater.py
+++ updater/readme-updater.py
@@ -3,20 +3,69 @@ import sys
 import json
 import requests
 import docker
+import argparse
+from dotenv import dotenv_values
+
+## global vars
+debug=False
 
 class env:
-    DOCKER_USER=''
-    DOCKER_PASS=''
-    PUSHRM_PROVIDER=dockerhub 
-    PUSHRM_FILE=/myvol/README.md 
-    PUSHRM_SHORT=''
-    PUSHRM_TARGET=''
-    PUSHRM_DEBUG=1 
-    def __init__(path):
-        with open(path, 'r') as f:
-            lines = f.read()
-            ## TODO: assign vars from file
+    DOCKER_USER=None
+    DOCKER_PASS=None
+    PUSHRM_PROVIDER='dockerhub'
+    PUSHRM_FILE='/myvol/README.md'
+    PUSHRM_SHORT=None
+    PUSHRM_TARGET=None
+    PUSHRM_DEBUG=1
 
+    def __init__(self, path):
+        self.get_common_env(path)
+
+
+    def get_common_env(self, path):
+        env_dict = {}
+        try:
+            environment = dotenv_values(dotenv_path=path)
+        except Exception as e:
+            print(f'Unhandled exception loading env at {path}:: \n\t{e}')
+        self.override_env(environment)
+        if debug:
+            print('Got common env values: ')
+            self.debug_env()
+        return environment
+
+    def override_env(self, environment):
+        for key in environment:
+            setattr(self, key, environment[key])
+
+    def debug_env(self):
+        hidden_pass = ''
+        if self.DOCKER_PASS and self.DOCKER_PASS != '':
+            for char in self.DOCKER_PASS:
+                hidden_pass += '*'
+
+        print(f'''
+            DOCKER_USER={self.DOCKER_USER}
+            DOCKER_PASS={hidden_pass}
+            PUSHRM_PROVIDER={self.PUSHRM_PROVIDER}
+            PUSHRM_FILE={self.PUSHRM_FILE}
+            PUSHRM_SHORT={self.PUSHRM_SHORT}
+            PUSHRM_TARGET={self.PUSHRM_TARGET}
+            PUSHRM_DEBUG={self.PUSHRM_DEBUG}
+            ''')
+        
+    def to_dict(self):
+        return {
+            "DOCKER_USER": f"{self.DOCKER_USER}",
+            "DOCKER_PASS": f"{self.DOCKER_PASS}",
+            "PUSHRM_PROVIDER": f"{self.PUSHRM_PROVIDER}",
+            "PUSHRM_FILE": f"{self.PUSHRM_FILE}",
+            "PUSHRM_SHORT": f"{self.PUSHRM_SHORT}",
+            "PUSHRM_TARGET": f"{self.PUSHRM_TARGET}",
+            "PUSHRM_DEBUG": f"{self.PUSHRM_DEBUG}"
+        }
+            
+    
 def get_map_from_file(path):
     with open(path, 'r') as file:
         data = file.read()
@@ -24,28 +73,84 @@ def get_map_from_file(path):
 
 def get_readme_from_url(url):
     resp = requests.get(url)
-    print(resp)
-    print(resp.text)
+    # print(resp)
+    # print(resp.text)
     return resp.text
 
-def get_common_env(path):
-
-    
-
-def update_readme(repo, readme):
+def update_readme(tmpfile_path, env):
+    if debug:
+        print(f'Updating Dockerhub repo: {env.PUSHRM_TARGET}')
     client = docker.from_env()
-    container = client.containers.run("chko/docker-pushrm", "/bin/sh", "-c", "/docker-pushrm", detach=True, environment=env)
-    print(f'have container {container}')
-    for line in container.logs(stream=True):
+    try:
+        container = client.containers.run("chko/docker-pushrm", detach=True, environment=env.to_dict(), volumes=[f'{tmpfile_path}:{env.PUSHRM_FILE}'])
+    except Exception as e:
+        print(f'Error trying to update readme: {e}')
+    if debug:
+        print(f'have container: {container.name}')
+    for line in container.logs(stream=True): 
         print(line.strip())
 
-def main():
+def main(env):
     mapping = get_map_from_file('map.json')
+    tmpfile_path = os.path.expanduser('~/tmp/dockerhubtmpfile.txt')
+    if debug:
+        print(f'Attempting to write readme contents to {tmpfile_path}')
     for repo in mapping:
-        readme = get_readme_from_url(mapping[repo])
-        update_readme(repo, readme)
+        # override the target repo in the env
+        dockerhub_target = mapping[repo].get('dockerhub_target')
+        readme_location = mapping[repo].get('readme_location')
+        short_desc = mapping[repo].get('short_desc')
+        overrides = {
+            "PUSHRM_TARGET": dockerhub_target,
+            "PUSHRM_SHORT": short_desc
+        }
+        env.override_env(overrides)
 
+        # save readme into tmp file for mounting in update_readme
+        readme = get_readme_from_url(readme_location)
+        with open(tmpfile_path, 'w+') as f:
+            f.write(readme)
+        
+        # do update
+        update_readme(tmpfile_path, env)
 
+        # cleanup
+        os.remove(tmpfile_path)
 
 if __name__ == '__main__':
-    main()
+    env_fpath = None
+    p = argparse.ArgumentParser()
+    p.add_argument('--env_file', '-f', dest='env_fpath', default='../../.env')
+    p.add_argument('--verbose', '-v', dest='verbose', action='store_true')
+    p.add_argument('--user', '-u', dest='DOCKER_USER')
+    p.add_argument('--password', '-p', dest='DOCKER_PASS')
+    p.add_argument('--provider', dest='PUSHRM_PROVIDER')
+    p.add_argument('--overview', '-o', dest='PUSHRM_FILE')
+    p.add_argument('--description', dest='PUSHRM_SHORT')
+    p.add_argument('--repo', '-r', dest='PUSHRM_TARGET')
+    args = p.parse_args()
+    globals()['debug'] = bool(args.verbose)
+
+    if args.env_fpath:
+        if debug:
+            print(f'have env file path: {args.env_fpath}')
+        env_fpath = os.path.expanduser(args.env_fpath)
+
+    environment = env(env_fpath)
+    overrides = {}
+    # dynamically assign args
+    # but skip verbose and env path
+    vars(args).pop('verbose')
+    vars(args).pop('env_fpath')
+    for arg in vars(args):
+        tmp = getattr(args, arg)
+        if tmp is not None:
+            overrides[arg] = tmp
+
+    # load override env with provided arg vals
+    if overrides is not {}:
+        if debug:
+            print(f'Overriding env vars with: {overrides}' if overrides != {} else 'No env overrides given')
+        environment.override_env(overrides)
+
+    main(environment)
